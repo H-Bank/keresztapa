8fcda6f646874807b572e777ebca9854838cd724
HBank <bank.horvath95@gmail.com>
2020-10-21 13:38:13 +0000

    pasz

---
+++ hello.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+    Console.WriteLine ("Hello World");
+    Console.WriteLine ("Hello World");
+  }
+}
+
